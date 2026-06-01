7f97cce70a6cfa339e1eb5964e7a35f7d758ca5a
José Manuel Pérez Gutiérrez <manuelprz0240@gmail.com>
2026-06-01 13:28:31 -0600

    feat: implement visitors dashboard page and add authentication middleware protection

--- .vercel/output/_functions/chunks/supabase_Du3HrT-j.mjs
+++
@@ -1,7 +0,0 @@
-import { createClient } from '@supabase/supabase-js';
-
-const supabaseUrl = "https://bczlxyzhhhspdgrrfvkr.supabase.co";
-const supabaseKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImJjemx4eXpoaGhzcGRncnJmdmtyIiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTc4MDI1NzE5MCwiZXhwIjoyMDk1ODMzMTkwfQ.kubPX_v7KhNu9jEW1EvGOBl-cHDo7T4kip2tUBM084c";
-const supabase = createClient(supabaseUrl, supabaseKey);
-
-export { supabase as s };
